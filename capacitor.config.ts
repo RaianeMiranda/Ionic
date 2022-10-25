15da5d83a7b71d525daf0d476bd606be79c18ef3
Raiane Miranda <raiane.oliveira@alunos.sc.senac.br>
2022-10-25 14:41:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RaianeApp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
